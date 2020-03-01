ae49bc848d84caa994121f656be2fc3ed12efe8a
Nathan Klassen <nthnklssn@live.com>
2020-03-01 13:25:29 -0500

    Created evaluate postion func

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 Main starting point for the chess program
 """
+import tools
 import chess
-
 board = chess.Board()
+tools.evaluatePosition(board)
--- tools.py
+++ tools.py
@@ -1,3 +1,30 @@
 """
 Chess Tools such as evaluating position/winning and losing go here
 """
+
+import chess
+
+
+"""
+Evaluate Position Function to determine the score of the position
+arg: Chess board
+
+Return: Number
+"""
+def evaluatePosition(board):
+    valueDict = {'P':1, 'p': -1, 'N': 3, 'n': -3, 'B': 3, 'b': -3,
+                 'R': 5, 'r': -5, 'Q': 9, 'q': -9, 'K': 10000, 'k':-10000}
+
+    pieceList = str(board).replace("/","").replace("\n"," ").replace(". ", "").split(" ")
+    print(pieceList)
+    pieceValues = [valueDict[pieceChar] for pieceChar in pieceList]
+    print(sum(pieceValues))
+
+"""
+Checks if game ends
+"""
+def gameEnd(board):
+    if(board.is_game_over() or board.is_sufficient_material() or board.can_claim_threefold_repetition()):
+        return -1
+    if(board.is_game_over()):
+        return 1
