f2411a49b0c2ba0b0797432dcaded0e2377e5efd
Christian Boin <christianboin@Christians-MacBook-Pro-2.local>
2020-03-01 15:20:53 -0500

    Added minimax search algorithm

--- main.py
+++ main.py
@@ -3,5 +3,33 @@ Main starting point for the chess program
 """
 import tools
 import chess
-board = chess.Board()
-tools.evaluatePosition(board)
+import sys
+
+
+"""
+Runs the main chess program
+
+Return: None
+"""
+def playChess():
+    board = chess.Board()
+    while True:
+        print(board.unicode())
+        flag = False
+        while not flag:
+            move = input("Make a move (e2e4): ")
+            try:
+                flag = chess.Move.from_uci(move) in board.legal_moves
+            except:
+                print("Invalid input. Try Again.")
+        # TODO: check for valid move
+        move = chess.Move.from_uci(str(move))
+        board.push(move)
+        print(board.unicode())
+        computerMove = tools.startMinimax(board, 4, True)
+        computerMove = chess.Move.from_uci(str(computerMove))
+        board.push(computerMove)
+
+if __name__=="__main__":
+
+    playChess()
